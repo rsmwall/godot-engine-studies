4abcdcb7a76325a1776f55a5240303101eb09ef2
Rafael Silva <rafaelrsilva.dev@gmail.com>
2022-08-29 16:29:43 -0300

    Add files via upload

--- godot-3x/gdscript-studies/GDScript Course/Scripts/EstruturasRepeticao.gd
+++ godot-3x/gdscript-studies/GDScript Course/Scripts/EstruturasRepeticao.gd
@@ -3,6 +3,17 @@ extends Node2D
 # for
 
 var x = 2
+var array = ['Rafael', 'Idinei', 'Brando', 'Carlos', 'Gabriela', 'Cassia']
+var dic = {
+    'pos1': 'filmes de acao',
+    'pos2': 'filmes de terror',
+    'pos3': 'filmes de aventura'
+}
+
+# while
+
+var j = 0
+var array2 = [10, 20, 30, 40 , 50, 60 ,70 , 80, 90, 100]
 
 func _ready():
 	for i in range(10): # incrementa i ate o valor especificado no range
@@ -19,3 +30,16 @@ func _ready():
     # exemplo 2 (decrementando)
 	for i in range(20, 0, -1):
 	    print(i)
+	
+	# listando elementos de um array
+	for i in range(array.size()):
+		print(array[i], ' posicao: ', i)
+	
+	# listando elementos de um dicionario
+	for elements in dic:
+		print(dic[elements])
+
+	# while para exibir valores de um array
+	while j < array.size():
+		print('posicao: ', j, ' -> valor: ', array[j])
+		j+=1
